64cb28b54df35254e8639fdf39e419ea02f1fd38
kalmk <111930167+kalmk@users.noreply.github.com>
2025-02-17 13:27:37 -0800

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -1,4 +1,7 @@
+keywords = ["int", "return", "if", "switch", "float", "while", "else", "case",
+            "char", "for", "goto", "unsigned", "main", "break", "continue", "void"]
+
+
 class lexer:
     def __init__(self):
         pass
-    
