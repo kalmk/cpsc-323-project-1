e6e8b4bc8b390aeb3ce37791789e57eddb6f1cd9
kalmk <111930167+kalmk@users.noreply.github.com>
2025-02-17 23:17:30 -0800

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,9 +1,9 @@
 def identifer_regex(word):
-    # Check if first char is ([a - Z])
+    # check if first char is ([a - Z])
     if not (word[0].isalpha()):
         return False
 
-    # Check if the rest of the word is ([a - Z] | [0 - 9])*
+    # check if the rest of the word is ([a - Z] | [0 - 9])*
     for char in word[1:]:
         if not (char.isalpha() or char.isdigit()):
             return False
@@ -12,11 +12,11 @@ def identifer_regex(word):
 
 
 def number_regex(word):
-    # Check if first char is ([0 - 9])
+    # check if first char is ([0 - 9])
     if not (word[0].isdigit()):
         return False
 
-    # Check if the rest of word is ([0 - 9])*
+    # check if the rest of word is ([0 - 9])*
     for char in word[1:]:
         if not (word[0].isdigit()):
             return False
@@ -25,11 +25,11 @@ def number_regex(word):
 
 
 def comment_regex(line):
-        # Check if the first 2 chars of the line is //
+        # check if the first 2 chars of the line is //
         if not line.startswith("//"):
             return False
 
-        # Check the end of the word for newline
+        # check the end of the word for newline
         if not line.endswith("\n"):
             return False
 
