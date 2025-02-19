fc4c24d8634fe030e99a264f25c98f960f414475
Ju Yeong Kim <111930167+kalmk@users.noreply.github.com>
2025-02-19 10:42:56 -0800

    Update test.py

--- test.py
+++ test.py
@@ -9,9 +9,12 @@ def scan_line():
 
     for i in lines[0]:
         dummy.append(i)
+
         if i == " ":
             dummy.pop()
+
         print(f"Dummy current: {dummy}")
+        
         if ''.join(dummy) in keywords:
             print("a keyword!")
             dummy.clear()
