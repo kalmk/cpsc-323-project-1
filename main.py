6a4d5957f8b25832e68d34ac48a1c85f6cad0aba
KshitijPingle <111930104+KshitijPingle@users.noreply.github.com>
2025-02-24 16:04:09 -0800

    Implemented the main loop
    I added many comments for reference and for people to see where I am imagining we call comments

--- main.py
+++ main.py
@@ -5,18 +5,50 @@ from scan_lines import *
 
 
 def main():
-    line = ""
+    token_stream = ""
     chars_scanned = ""
 
-    # Current state can be derived by columns, q0 = [0, 0]
+    # Call function to make a copy of our DFA here
+    # Ex. our_dfa = make_dfa()
+
+    # Current state can be derived by rows and columns, q0 = row 0
     current_row = 0
     current_column = 0
-
-    while (True):
-        break
+    token = ""
 
     input_lines = scan_lines("input.in")
-    print(input_lines)
+    # print(input_lines)
+
+    while (True):
+        # Go line by line
+        for line in input_lines:
+
+            for char in line:
+                # Now we are reading each character in each line one by one
+
+                if ((char == "\n") or (char == " ")):
+                    continue
+                
+                reject = False
+                # call function to change state according to input
+                # Ex. reject, curret_row, current_column = change_state(char, current_row, current_column)
+
+                if (reject):
+                    # First, get back the appropriate token for chars_scanned
+                    # Ex. token = get_token(current_row, current_column)
+                    token_stream += token
+
+                    token = ""
+                    chars_scanned = ""
+                    continue
+
+                # If our dfa didn't reject, append the char
+                chars_scanned += char
+
+                print(char)
+
+        
+        break
 
 if __name__ == "__main__":
     main()
