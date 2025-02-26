f80a68b2af62d269e7bbe88c3ac546b75c8843a5
nwindly <nwindly@gmail.com>
2025-02-25 17:35:01 -0800

    Update dfa.py

--- dfa.py
+++ dfa.py
@@ -1,43 +1,43 @@
-states = ["q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11", "q12", "q13", "q14", "q15", "q16", "q17", "q18", "q19", "q20", "q21", "q22", "q23", "q24", "q25", "q26", "q27", "q28", "q29", "q30", "q31"]
 
-token_names = ["identifier", "number", "comment", "leftParen", "rightParen", "leftBracket", "rightBracket", "leftBrace", "rightBrace", "dot", "plus", "minus", "multiply", "divide", "modulus", "lessThan", "greaterThan", "assignment", "semicolon", "comma", "increment", "decrement", "lessThanEq", "greaterThanEq", "logicEqual", "logicANd", "logicOr", "logicNot", "bitAnd", "bitOr"]
+
+token_names = ["identifier", "number", "comment", "leftParen", "rightParen", "leftBracket", "rightBracket", "leftBrace", "rightBrace", "dot", "plus", "minus", "multiply", "divide", "modulus", "lessThan", "greaterThan", "assignment", "semicolon", "comma", "increment", "decrement", "lessThanE", "greaterThanE", "logicEual", "logicANd", "logicOr", "logicNot", "bitAnd", "bitOr"]
 
 token_keywords = ["int", "return", "if", "switch", "float", "while", "else", "case", "char", "for", "goto", "unsigned", "main", "break", "continue", "void"]
 
 dfa_table = [
-    #   a-Z   0     1-9   -     .     !     =     +     <     >     &     |     (     )     {     }     *     /     ;
-    ["q1", "q5", "q3", "q4", None, "q9", "q29", "q11", "q14", "q16", "q18", "q20", "q22", "q23", "q24", "q25", "q26", "q27", "q31"],  # q0
-    ["q2", "q2", "q2", None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q1
-    ["q2", "q2", "q2", None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q2
-    [None, "q3", "q3", None, "q7", None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q3
-    [None, "q6", "q3", "q13", None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q4
-    [None, None, None, "q7", None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q5
-    [None, None, None, "q7", None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q6
-    ["q8", "q8", "q8", None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q7
-    ["q8", "q8", "q8", None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q8
-    [None, None, None, None, None, "q10", None, None, None, None, None, None, None, None, None, None, None, None, None],  # q9
-    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q10
-    [None, None, None, None, None, None, "q12", None, None, None, None, None, None, None, None, None, None, None, None],  # q11
-    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q12
-    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q13
-    [None, None, None, None, None, None, "q15", None, None, None, None, None, None, None, None, None, None, None, None],  # q14
-    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q15
-    [None, None, None, None, None, None, "q17", None, None, None, None, None, None, None, None, None, None, None, None],  # q16
-    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q17
-    [None, None, None, None, None, None, None, None, None, None, "q19", None, None, None, None, None, None, None, None],  # q18
-    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q19
-    [None, None, None, None, None, None, None, "q21", None, None, None, None, None, None, None, None, None, None, None],  # q20
-    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q21
-    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q22
-    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q23
-    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q24
-    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q25
-    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q26
-    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, "q28", None],  # q27
-    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q28
-    [None, None, None, None, None, None, "q30", None, None, None, None, None, None, None, None, None, None, None, None],  # q29
-    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q30
-    ["q31", None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # q31
+    #  a-Z   0-9   -    !    =    +    <    >    &    |    (    )    {    }    *    /    ;    .    [    ]    ,    %
+    ["1",  "4",  "2",  "5",  "9",  "7",  "11", "13", "15", "17", "19", "20", "21", "22", "23", "24", "26", "27", "28", "29", "30", "31"],  # 0
+    ["1",  "1",  None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 1 Identifier/Variable
+    [None, "4",  "3",  None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 2 Minus
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 3 Decrement Operator
+    [None, "4",  None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 4 Numbers/Integer
+    [None, None, None, None, "6", None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 5 Logical Not Operator
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 6 Not Equal Operator
+    [None, None, None, None, None, "8", None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 7 Plus
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 8 Increment Operator
+    [None, None, None, None, "10", None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 9 Assignment Operator
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 10 Equality Comparison Operator
+    [None, None, None, None, None, None, "12", None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 11 Less than
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 12 Less than, Equal to
+    [None, None, None, None, None, None, None, "14", None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 13 Greater than
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 14 Greater than, Equal to
+    [None, None, None, None, None, None, None, None, "16", None, None, None, None, None, None, None, None, None, None, None, None, None],  # 15 Bit And
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 16 Logic And
+    [None, None, None, None, None, None, None, None, None, "18", None, None, None, None, None, None, None, None, None, None, None, None],  # 17 Bit Or
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 18 Logic Or
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 19 Left Parenthesis
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 20 Right Parenthesis
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 21 Left Curly Brace
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 22 Right Curly Brace
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 23 Multiplication Operator
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, "25", None, None, None, None, None],  # 24 Division
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 25 Comment
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 26 Semicolon
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 27 Dot
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 28 Left Bracket
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 29 Right Bracket
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 30 Comma
+    [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],  # 31 Modulus
 ]
 
 # need a current state and input variables to keep track
