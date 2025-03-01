e9c6d843454634deee485c59150886690ab0cacb
KshitijPingle <111930104+KshitijPingle@users.noreply.github.com>
2025-02-28 16:25:45 -0800

    Fixed issue with next line messing things up

--- main.py
+++ main.py
@@ -22,11 +22,10 @@ def main(filename):
             comment_detected = False
 
             for char in line:
+                comment_detected = False
                 # Now we are reading each character in each line one by one
                 if ((char == "\n") and (len(chars_scanned) == 1)):
                     reject = True
-                elif (char == "\n"):
-                    continue
 
                 
                 reject = False
@@ -46,6 +45,7 @@ def main(filename):
                     if is_keyword != "IDENTIFIER":
                         token = is_keyword
                     token_stream = token_stream + " " + token
+                    # DEBUG: print(f"For '{chars_scanned}', saved '{token}' to token_stream")
 
                     if (token == "comment"):
                         token = ""
@@ -78,9 +78,11 @@ def main(filename):
         if is_keyword != "IDENTIFIER":
             token = is_keyword
         token_stream = token_stream + " " + token
+        # DEBUG: print(f"For '{chars_scanned}', saved '{token}' to token_stream")
 
     print("Token stream:", token_stream)
 
 if __name__ == "__main__":
     main("input.in")
-    main("test_1.in")
+    print()
+    main("case1.txt")
